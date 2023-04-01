2da2a935109553eb5b9ab9734baaaa575afb192f
AX <ananya.dhananjaya1998@gmail.com>
2023-04-02 01:14:28 +0530

    feat: navigation, sidebar, main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { useState } from 'react'
 import Sidebar from './Sidebar/Sidebar'
+import About from './About/About'
+import Home from './Home/Home'
+import Works from './Works/Works'
 
 function App() {
-  return <Sidebar />
+  const [page, setPage] = useState<string>('')
+
+  const renderPage = (page: string) => {
+    switch (page) {
+      case 'About':
+        return <About />
+      case 'Works':
+        return <Works />
+      default:
+        return <Home />
+    }
+  }
+  return (
+    <>
+      <div className="h-screen">
+        <div className="flex items-center h-2/3 flex-col md:flex-row justify-center">
+          <Sidebar page={page} setPage={setPage} />
+          {renderPage(page)}
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App
