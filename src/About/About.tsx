7e5f8f4516c39174e150674dbc5fda70d85e528d
AX <ananya.dhananjaya1998@gmail.com>
2023-04-02 22:10:15 +0530

    feat: About content

--- src/About/About.tsx
+++ src/About/About.tsx
@@ -1,7 +1,34 @@
-import { Links } from '../Misc/Links'
-
 const About = () => {
-  return <div className="m-6 min-w-xl w-7/12">test</div>
+  return (
+    <div className="m-6 min-w-xl max-w-xl w-7/12">
+      <div className="text-3xl">About Me</div>
+      <br />
+      <div>
+        As a <b>Senior Frontend Engineer at Edstruments</b>, I play a crucial
+        role in building and maintaining the company&apos;s Budget Intuitive
+        Management App. My focus is on writing clean and efficient code, as well
+        as implementing new methods for code maintenance, refactoring, and
+        documentation, as it is essential for the success of the application.
+      </div>
+      <div className="h-px bg-slate-300 my-10"></div>
+      <div>
+        I am passionate about a vareity of creative persuits, including physics,
+        urban sketching, cooking and coding.
+        <div className="m-3" />
+        One of my greatest joys is building for the web. Whether it&apos;s a
+        simple HTML file or a new framework, the web never fails to amaze me.
+        It&apos;s boundless potential means that anyone can become a developer,
+        writer, or creator. All you have to do is start building.
+        <div className="m-3" />I have immense respect for solopreneurs and
+        open-source contributors, and I draw inspiration from them. The
+        dedication and passion that these individuals bring to their work never
+        ceases to amaze me. As someone who values creativity and independence, I
+        find their stories incredibly inspiring, and I strive to emulate their
+        success in my own endeavors.I am inspired by the tireless efforts of
+        those who pave their own way in the world.
+      </div>
+    </div>
+  )
 }
 
 export default About
